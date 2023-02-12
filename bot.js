a939f2f46011d0f2304c73bf453492cc0518aa05
Arasya Rafi <94653668+GetSya@users.noreply.github.com>
2023-02-12 07:42:52 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -43,9 +43,9 @@ Kirimkan pertanyaan kamu disini, nanti bot akan menjawab pertanyaan kamu.
 
 https://t.me/${bot.botInfo.username.toLowerCase()}`;
 
-    reply(chatId, `Hai 👋\n\nSaya adalah Robot AI untuk menjawab pertanyaan anda, Silahkan kirim Pertanyaan kamu, nanti jawaban kamu akan dijawab oleh robot.\n\n_AI (Artificial Intelligence) adalah teknologi yang menggunakan algoritma kompleks untuk membuat mesin yang dapat berpikir dan bertindak seperti manusia. AI dapat digunakan untuk menyelesaikan masalah yang rumit dan membuat keputusan yang lebih tepat daripada manusia. AI juga dapat digunakan untuk menganalisis data dan mengambil keputusan berdasarkan data tersebut. AI juga dapat digunakan untuk meningkatkan produktivitas dan efisiensi, serta membantu manusia dalam menyelesaikan tugas-tugas yang rumit._\n\n_bot dibatasi menjawab maximal ${MAX_TOKEN} kata_\n\n*Created by @caliphdev*`, messageId, { parse_mode: "Markdown", reply_markup: {
+    reply(chatId, `Hai 👋\n\nSaya adalah Robot AI untuk menjawab pertanyaan anda, Silahkan kirim Pertanyaan kamu, nanti jawaban kamu akan dijawab oleh robot.\n\n_AI (Artificial Intelligence) adalah teknologi yang menggunakan algoritma kompleks untuk membuat mesin yang dapat berpikir dan bertindak seperti manusia. AI dapat digunakan untuk menyelesaikan masalah yang rumit dan membuat keputusan yang lebih tepat daripada manusia. AI juga dapat digunakan untuk menganalisis data dan mengambil keputusan berdasarkan data tersebut. AI juga dapat digunakan untuk meningkatkan produktivitas dan efisiensi, serta membantu manusia dalam menyelesaikan tugas-tugas yang rumit._\n\n_bot dibatasi menjawab maximal ${MAX_TOKEN} kata_\n\n*Created by @sofunsyabi*`, messageId, { parse_mode: "Markdown", reply_markup: {
     inline_keyboard: [
-      [{ text: '💌 Owner', url: "tg://user?id="+OWNER_ID[0] }, { text: "🔗 Source Code", url: "https://github.com/caliphdev/openai-telebot" }],
+      [{ text: '💌 Owner', url: "https://wa.me/"+OWNER_ID[0] }, { text: "🔗 Source Code", url: "https://github.com/GetSya/tele" }],
       [{ text: "❤️ Share Bot ini", url: "https://t.me/share/url?"+new URLSearchParams({ text: shareText }) }]
     ]
   } });
